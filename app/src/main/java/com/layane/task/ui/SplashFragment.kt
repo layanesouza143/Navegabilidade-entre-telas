4baf41bbd48a077eb7336f80487b41ac47302c14
layaneSouza <layane.fsouza14@gmail.com>
2026-05-12 11:19:06 -0300

    Atualização1

--- app/src/main/java/com/layane/task/ui/SplashFragment.kt
+++ app/src/main/java/com/layane/task/ui/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.layane.task.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.layane.task.R
 import com.layane.task.databinding.FragmentSplashBinding
 
@@ -21,6 +24,18 @@ class SplashFragment : Fragment() {
 
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            checkAuth()
+        }, 3000)
+    }
+
+    private fun checkAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_authentication2)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/layane/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/layane/task/ui/auth/LoginFragment.kt
@@ -1,18 +1,48 @@
 package com.layane.task.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.layane.task.R
+import com.layane.task.databinding.FragmentLoginBinding
+
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListener()
+    }
+
+    private fun initListener() {
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
+        binding.btnRecover.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
